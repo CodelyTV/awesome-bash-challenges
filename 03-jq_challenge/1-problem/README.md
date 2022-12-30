4fe1b39b79d536ff6acbb6caebc647ed032dc6e9
Rafa Gómez <rgomezcasas@gmail.com>
2022-12-30 09:37:15 +0100

    feat: add 3rd lesson

---
+++ 03-jq_challenge/1-problem/README.md
@@ -0,0 +1,2 @@
+# 🔥 Reto
+* Queremos saber quién es la persona que más colabora en nuestro repo de `dotly`
--- _shared/compare_json.sh
+++ _shared/compare_json.sh
@@ -3,5 +3,5 @@ function it_should_output_the_expected_json() {
 	expected_output_file="$2"
 	actual_output=$(sh "$executor_file")
 
-	diff <(echo "$actual_output" | jq --sort-keys .) <(jq --sort-keys . "$expected_output_file") && echo "✅ All ok!" || echo "❌ The output is not the expected"
+	diff <(echo "$actual_output" | jq --sort-keys .) <(jq --sort-keys . "$expected_output_file") && echo "✅ All ok!" && exit 0 || echo "❌ The output is not the expected" && exit 1
 }
