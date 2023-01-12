3b30700e5640a02a85ccb241284ae2935ee6445f
Rafa Gómez <rgomezcasas@gmail.com>
2023-01-12 16:29:06 +0100

    feat: add 02/2 example

--- 02-jq/2-combine_filter_json/execute.sh
+++ 02-jq/2-combine_filter_json/execute.sh
@@ -1,1 +1,1 @@
-jq '' "$PWD/_input.json"
+jq '[.[] | {titulo: (.emoji + " " + .title), titulo_largo: (if .title | length > 50 then "si" else "no" end)} | select(.titulo | length > 40)]' "$PWD/_input.json"
