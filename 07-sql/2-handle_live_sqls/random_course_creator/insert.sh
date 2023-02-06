4dd1a8581b8d7d64a2a9f2b9beec6a444469c6c2
Rafa Gómez <rgomezcasas@gmail.com>
2023-02-06 12:06:23 +0100

    feat: add script to generate random courses

---
+++ 07-sql/2-handle_live_sqls/random_course_creator/insert.sh
@@ -0,0 +1,22 @@
+function random_course_name() {
+	shuf -n4 ./courses_words.txt | xargs echo
+}
+
+function random_duration() {
+	echo $((RANDOM % 50 + 1)) "hours"
+}
+
+function generate_random_course_insert() {
+	course_id=$(uuidgen)
+	course_name=$(random_course_name)
+	course_duration=$(random_duration)
+
+	insert_sql="INSERT INTO courses (id, name, duration) VALUES ('$course_id', '$course_name', '$course_duration');"
+
+	echo "$insert_sql"
+}
+
+while true; do
+	generate_random_course_insert
+	sleep  1
+done
